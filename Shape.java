32d7b3031b91e1351dfb365ff18f65beabd7c776
fajril000 <114728478+fajril000@users.noreply.github.com>
2024-03-06 12:16:06 +0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+    protected double area;
+    protected double perimeter;
+
+    public void calculateArea() {
+        System.out.println("Area: " + area);
+    }
+
+    public void calculatePerimeter() {
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
